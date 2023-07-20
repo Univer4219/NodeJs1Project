462a00e717a20d34f79f7462db58d83dd64c82ab
Univer4219 <108887059+Univer4219@users.noreply.github.com>
2023-07-20 23:53:04 +0300

    superheores project

---
+++ index.js
@@ -0,0 +1,14 @@
+// import generateName from 'sillyname';
+// var sillyName = generateName();
+
+// // import generateName from "sillyname"
+
+// console.log(`My name is ${sillyName}.`);
+
+const superheroes = require('superheroes');
+ 
+superheroes.all;
+superheroes.random();
+
+console.log(`I am ${superheroes}`);
+ 
